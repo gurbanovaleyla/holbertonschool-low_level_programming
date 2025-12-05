fe435a02fea497a09cace22d02c63230841ce917
Your Name <your@email.com>
2025-12-05 11:24:38 +0100

    Printing alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
