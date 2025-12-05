23fd54ef633cfa6d74fb7deb6026f695b1bd86a7
Your Name <your@email.com>
2025-12-05 12:19:16 +0100

    Printing alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+
+		_putchar('\n');
+	}
+}
