42faf1b0793455857d03ccb5037e54107f5a5d6e
Your Name <your@email.com>
2025-12-05 19:41:21 +0100

    Multiplication of two numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul(int a, int b) - multiples two digits
+* @a: first integer
+* @b: second integer
+* mul: multiplication of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
