f192f6e5187693c5b93ba00faea7c2fe7b2386a9
Your Name <your@email.com>
2025-12-05 18:12:36 +0100

    Printing uppercase letters

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to check
- *
- * Return: 1 if uppercase, 0 otherwise
- */
+* _isupper - checks for uppercase character
+* @c: the character to check
+*
+* Return: 1 if uppercase, 0 otherwise
+*/
 
 int _isupper(int c)
 {
