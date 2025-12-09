f6f202a15440f9edddc57af06c030ae0241f80b6
Your Name <your@email.com>
2025-12-09 16:18:37 +0100

    Converting a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,28 +2,25 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: pointer to the string to convert
+ * @s: the string to convert
  *
- * Return: the integer value of the string
+ * Return: the integer value
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
-	
-	/* Skip non-digit characters until we find a number or signs */
+	int i = 0, sign = 1, result = 0, found = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (found)
 			break;
-		i++;
-	}
-
-	/* Convert digits to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
