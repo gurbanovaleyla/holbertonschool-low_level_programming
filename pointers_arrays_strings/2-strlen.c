f6ed81ebb33015373738fc3be1c313b63eeb0ab0
Your Name <your@email.com>
2025-12-09 12:48:14 +0100

    Finding the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of the string
+* @s: pointer to the string
+*
+* Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
