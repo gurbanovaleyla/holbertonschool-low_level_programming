0070d995b0176c97b1d1ab298945ad54de6d974e
Your Name <your@email.com>
2025-12-09 16:23:00 +0100

    Converting a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,16 +2,15 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: pointer to the string
+ * @s: string to convert
  *
- * Return: the converted integer
+ * Return: converted integer
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1;
-	unsigned int result = 0;
+	int i = 0, sign = 1, num = 0;
 
-	/* Process all characters until a digit */
+	/* process signs before the number */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
@@ -21,15 +20,15 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* Convert digits */
+	/* convert digits to integer */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		if (sign == 1)
+			num = num * 10 + (s[i] - '0');
+		else
+			num = num * 10 - (s[i] - '0');
 		i++;
 	}
 
-	/* Apply sign safely */
-	if (sign < 0)
-		return (-((int)result));
-	return ((int)result);
+	return (num);
 }
