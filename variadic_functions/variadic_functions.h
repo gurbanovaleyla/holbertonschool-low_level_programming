a04a3aeeefa6f9b90c83cfbe95d8a9114685851e
Your Name <your@email.com>
2026-01-07 16:50:53 +0100

    files added

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Prototype */
+int sum_them_all(const unsigned int n, ...);
+
+/* _putchar prototype (lazımdır) */
+int _putchar(char c);
+
+#endif
