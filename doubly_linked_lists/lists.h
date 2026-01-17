6d83c610cfdaaa7150610dfa5066ce6a74621770
Your Name <your@email.com>
2026-01-17 15:27:23 +0100

    files added

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -36,4 +36,6 @@ size_t print_dlistint(const dlistint_t *h);
 
 void free_dlistint(dlistint_t *head);
 
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
 #endif /* LISTS_H */
