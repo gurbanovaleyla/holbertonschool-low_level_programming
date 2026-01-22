eb32fe5530523981bff82478b7b6cbc29cd1dec6
Your Name <your@email.com>
2026-01-22 17:52:03 +0100

    files added

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: the name of the file
+* @letters: number of the letters to read and print
+*
+* Return:  actual number of letters it could read and print, or 0 in failure
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (bytes_written);
+}
