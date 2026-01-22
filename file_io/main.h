036e82501c428fa08c63eb608c67e1b0e5c17fa8
Your Name <your@email.com>
2026-01-22 17:59:19 +0100

    files added

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <unistd.h>   /* read, write, close */
+#include <fcntl.h>    /* open, O_RDONLY */
+#include <stdlib.h>   /* malloc, free */
 
+/* Task prototype */
 ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
 
 #endif /* MAIN_H */
