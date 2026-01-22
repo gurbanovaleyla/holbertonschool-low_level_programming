54f7f67ec89beb0e9295922a28bc24c014280649
Your Name <your@email.com>
2026-01-22 19:49:51 +0100

    files added

--- file_io/main.h
+++ file_io/main.h
@@ -12,4 +12,6 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void copy_file(const char *file_from, const char *file_to);
+
 #endif /* MAIN_H */
